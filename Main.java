f41a5c8ae2d0876b9bfaf2c0e8707ba70c99339e
skarjala <68705118+skarjala@users.noreply.github.com>
2021-12-24 11:29:45 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,53 +1,57 @@
 //package com.sandeep;
+
 import java.io.*;
-import java.util.*;
-import java.lang.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader( new FileReader("shell.in"));
-        PrintWriter pw = new PrintWriter( new FileWriter("shell.out"));
-        //Scanner sc = new Scanner(System.in);
-        int shell1 = 0;
-        int shell2 = 0;
-        int shell3 = 0;
-        int tmp;
-        int a;
-        int b;
-        int g;
-        int[] arr = new int[3];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-
-        StringTokenizer st = new StringTokenizer(r.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        for (int i=0; i < n; i++) {
-            st = new StringTokenizer(r.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            g = Integer.parseInt(st.nextToken());
-            tmp = arr[a-1];
-            arr[a-1]= arr[b-1];
-            arr[b-1] = tmp;
-
-            if (arr[g-1] == 1) {
-                shell1++;
+    public static int tarp (int x1,int y1,int x2,int y2, int bx, int by, int tx, int ty) {
+        int area = 0;
+        int width = 0;
+        int length = 0;
+        if (tx <= x1 || bx >= x2 || ty <= y1 || by >= y2) {
+            area = (x2-x1) * (y2-y1);
+            return area;
+        } else {
+            if (tx >= x2 && bx <= x1 && ty >= y2 && by <= y1) {
+                area = 0;
+                return area;
             }
-            if (arr[g-1] == 2) {
-                shell2++;
-            }
-            if (arr[g-1] == 3) {
-                shell3++;
+            else if ((tx >= x2 && bx <= x1) || (ty >= y2 && by <= y1)) {
+
+                if ((tx >= x2 && bx <= x1 && ty <= y2 && by >= y1) || (ty >= y2 && by <= y1 && tx <= x2 && bx >= x1)) {
+                    area = (x2 - x1) * (y2 - y1);
+                    return area;
+                } else {
+                    width = Math.min(x2, tx) - Math.max(x1, bx);
+                    length = Math.min(y2, ty) - Math.max(y1, by);
+                    area = (x2 - x1) * (y2 - y1) - (width * length);
+                    return area;
+                }
+            } else {
+                area = (x2-x1) * (y2-y1);
+                return area;
             }
         }
-        int maxPts = Math.max(shell1, Math.max(shell2,shell3));
-        pw.print(maxPts);
+    }
 
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader r = new BufferedReader( new FileReader("billboard.in"));
+        PrintWriter pw = new PrintWriter( new FileWriter("billboard.out"));
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(r.readLine());
+
+        int bx = Integer.parseInt(st.nextToken());
+        int by = Integer.parseInt(st.nextToken());
+        int tx = Integer.parseInt(st.nextToken());
+        int ty = Integer.parseInt(st.nextToken());
+        pw.print(tarp(x1,y1,x2,y2,bx,by,tx,ty));
         r.close();
         pw.close();
-
-
     }
 }
